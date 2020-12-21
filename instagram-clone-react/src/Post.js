2c8a42dc73d67694901b1ffe55f7cb928737aebf
hi <kangja1996@gmail.com>
2020-12-20 22:05:28 -0500

    progress

---
+++ instagram-clone-react/src/Post.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      <h3>Username</h3>
+      {/* header -> avatar+ username */}
+
+      <img
+        src="https://www.google.com/logos/doodles/2020/remembering-sudan-the-last-male-northern-white-rhino-6753651837108653-l.png"
+      />
+      {/* image */}
+
+      <h4>Username: caption</h4>
+      {/* username + caption */}
+    </div>
+  )
+}
+
+export default Post
