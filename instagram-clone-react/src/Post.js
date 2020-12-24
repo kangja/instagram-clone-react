56089ab0bb511b5acf1888538dddc58d89c5b65d
hi <kangja1996@gmail.com>
2020-12-23 23:13:19 -0500

    progress

--- instagram-clone-react/src/Post.js
+++ instagram-clone-react/src/Post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post( {username, caption, imageUrl}) {
   return (
     <div className="post">
       <div className="post__header">
@@ -11,18 +11,18 @@ function Post() {
           alt="Jason"
           src="/static/images/avatar/1.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
         {/* header -> avatar+ username */}
       </div>
       
 
       <img
         className="post__image"
-        src="https://www.google.com/logos/doodles/2020/remembering-sudan-the-last-male-northern-white-rhino-6753651837108653-l.png"
+        src={imageUrl}
       />
       {/* image */}
 
-      <h4 className="post__text"><strong>jasonKang: </strong>feeling good</h4>
+      <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
       {/* username + caption */}
     </div>
   )
