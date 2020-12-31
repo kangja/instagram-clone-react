1ea313e2264cc988634f8d0bd6921f70e98b726e
hi <kangja1996@gmail.com>
2020-12-31 01:09:42 -0500

    progress

--- instagram-clone-react/src/App.js
+++ instagram-clone-react/src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from "./firebase"
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
 
 function getModalStyle() {
   const top = 50;
@@ -32,9 +33,9 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
-
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -83,11 +84,31 @@ function App() {
         displayName:   username
       })
     })
+      .catch((error) => alert(error.message))
+    
+    setOpen(false);
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
     .catch((error) => alert(error.message))
+
+    setOpenSignIn(false);
   }
 
   return (
     <div className="app">
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+        
+
+      ) : (
+          <h3>Sorry, you need to login to upload.</h3>
+      )}
+
       <Modal
         open={open}
         onClose={() => setOpen(false)}
@@ -127,6 +148,40 @@ function App() {
       </div>
       </Modal>
 
+<Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+      >
+              
+        <div style={modalStyle} className={classes.paper}>
+          
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button type="submit" onClick={signIn}>Sign In</Button>
+            </form>
+      </div>
+      </Modal>
+
+
 
       <div className="app__header">
         <img 
@@ -139,7 +194,10 @@ function App() {
       {user ? (
         <Button onClick={() => auth.signOut()}>Logout</Button>
       ): (
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
         )}
       
        <h1>Hello my name is jason</h1>
